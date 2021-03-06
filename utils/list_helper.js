148c6af6befb77799244ed09c321f32b9c4f5221
dg0397 <rgg0397@gmail.com>
2021-03-06 18:51:32 -0400

    4.4: helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,12 @@ const dummy = (blogs) => {
   return 1
 };
 
+const totalLikes = (blogs) => {
+    const allLikes = blogs.map(blog => blog.likes);
+    return allLikes.reduce((a,b) => a+b,0)
+}
+
 module.exports = {
   dummy,
+  totalLikes
 };
