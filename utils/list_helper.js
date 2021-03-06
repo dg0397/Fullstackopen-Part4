8992f4b0333f45dcc66d0464dd8c9d1e23cf9395
dg0397 <rgg0397@gmail.com>
2021-03-06 19:26:03 -0400

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,14 +1,27 @@
 const dummy = (blogs) => {
   // ...
-  return 1
+  return 1;
 };
 
 const totalLikes = (blogs) => {
-    const allLikes = blogs.map(blog => blog.likes);
-    return allLikes.reduce((a,b) => a+b,0)
-}
+  const allLikes = blogs.map((blog) => blog.likes);
+  return allLikes.reduce((a, b) => a + b, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  const allLikes = blogs.map((blog) => blog.likes);
+  const maxNumberOfLikes = Math.max(...allLikes);
+  const blogMostLiked = blogs.find((blog) => blog.likes === maxNumberOfLikes);
+  if (blogMostLiked) {
+    const { title, author, likes } = blogMostLiked;
+    return { title, author, likes };
+  } else {
+    return;
+  }
+};
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
 };
