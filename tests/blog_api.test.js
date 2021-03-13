94ff1f6de5e4a2d8eb14fcdf71245f3181e350d2
dg0397 <rgg0397@gmail.com>
2021-03-13 11:24:09 -0400

    4.11*: Blog list tests, step4

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -52,8 +52,27 @@ test('a valid blog can be added', async () => {
     const blogsAtEnd = await helper.blogsInDb()
     expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 
-    const contents = blogsAtEnd.map(blog => blog.title)
-    expect(contents).toContain('The Next Mission')
+    const titles = blogsAtEnd.map(blog => blog.title)
+    expect(titles).toContain('The Next Mission')
+})
+
+test('likes property will be 0 by default', async () => {
+    const newBlog = {
+        title:'The Next Mission',
+        author:"Brendan Eich",
+        url:'https://brendaneich.com/2014/04/the-next-mission/'
+    }
+    
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type',/application\/json/)
+
+        const blogsAtEnd = await helper.blogsInDb()
+        const [lastBlog] = blogsAtEnd.slice(-1);
+
+        expect(lastBlog.likes).toBe(0)
 })
 
 afterAll(() => {
